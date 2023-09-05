bfb42e27d2f775cbac8f2e8b2392a6e88a5b0adb
KshitijMagare19 <kshitij.magare19@vit.edu>
2023-09-06 02:33:42 +0530

    LinkedList

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -10,6 +10,72 @@ public class LL {
         this.size = 0;
     }
 
+    public void display() {
+        Node t = head;
+        while(t!=null){
+            System.out.print(t.val+ " -> ");
+            t = t.next;
+        }
+        System.out.print("null\n");
+    }
+    public void insertFirst(int val) {
+        Node nn = new Node(val);
+        nn.next = head;
+        head = nn;
+        
+        if(tail == null){
+            tail = head;
+        }
+        size++;
+    }
+    public void insertLast(int val) {
+        Node nn = new Node(val);
+        tail.next = nn;
+        tail = nn;
+        
+        if(tail == null){
+            insertFirst(val);
+        }
+        size++;
+    }
+
+    public void insertLastWithoutTail(int val) {
+        Node nn = new Node(val);
+        Node t = head;
+        while(t.next!=null){
+            t = t.next;
+        }
+        t.next = nn;
+        tail = nn;
+        
+        if(tail == null){
+            insertFirst(val);
+        }
+        size++;
+    }
+    public void deleteFirst() {
+        if(head == null){
+            tail = null;
+            System.out.println("No element prensent for deletion");
+        }
+        Node t = head;
+        head = t.next;    
+        size--;
+    }
+    public void deleteLast() {
+        if(head == null){
+            System.out.println("No element prensent for deletion");
+        }
+        Node t1 = head;
+        Node prev = null;
+        while(t1.next!=null){
+            prev = t1;
+            t1 = t1.next;
+        }
+        prev.next = null;
+        tail = prev;
+        size--;
+    }
     private class Node {
         private int val;
         private Node next;
@@ -25,7 +91,23 @@ public class LL {
 
     }
     public static void main(String[] args) {
-        
+        LL singlyLinkedList = new LL();
+        singlyLinkedList.insertFirst(10);
+        singlyLinkedList.insertFirst(20);
+        singlyLinkedList.insertFirst(30);
+        singlyLinkedList.insertFirst(40);
+        singlyLinkedList.insertLast(55);
+        singlyLinkedList.insertLastWithoutTail(68);
+        singlyLinkedList.insertFirst(45);
+
+        singlyLinkedList.display();
+
+        singlyLinkedList.deleteFirst();
+        singlyLinkedList.display();
+
+        singlyLinkedList.deleteLast();
+        singlyLinkedList.display();
+
     }
 
 }
